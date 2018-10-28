10b295e7d652da1ed6b7c4008fb99a22fba786f4
pcharishma <charishma2302@gmail.com>
2018-10-28 14:58:38 -0700

    Output is sent to the text files. CSV is reading properly. Code works well for different column names. TODO: outfiles should go to the output directory

--- src/top10occupations.py
+++ src/top10occupations.py
@@ -1,42 +1,61 @@
 import os
 import csv
 import sys
-import regex as re
-
 
 def main(argv):
+    csv.field_size_limit(100000000)
     input_file = argv[0]
-    print(input_file)
     result_data = []
-    with open(input_file,'r') as csvfile:
+    with open(input_file, 'r', errors='ignore') as csvfile:
         input_data = csv.reader(csvfile)
         for row in input_data:
-            temp = [', '.join(row)][0].split(';')
-            #temp1 = re.split(r'\n[0-9]',temp)
-            #print(temp1)
+            temp = "".join(row).split(';')
             result_data.append(temp)
-
     col_header = result_data[0]
     row_data = result_data[1:]
 
     # Column numbers to group data on.
-    status = col_header.index('CASE_STATUS')
-    occupation = col_header.index('SOC_NAME')
-    work_state = col_header.index('WORKSITE_STATE')
+    if 'CASE_STATUS' in col_header:
+        status = col_header.index('CASE_STATUS')
+    elif 'STATUS' in col_header:
+        status = col_header.index('STATUS')
+    else:
+        print("error message")
+
+    if 'SOC_NAME' in col_header:
+        occupation = col_header.index('SOC_NAME')
+    elif 'LCA_CASE_SOC_NAME' in col_header:
+        occupation = col_header.index('LCA_CASE_SOC_NAME')
+    else:
+        print("error message")
+
+    if 'WORKSITE_STATE' in col_header:
+        work_state = col_header.index('WORKSITE_STATE')
+    elif 'LCA_CASE_EMPLOYER_STATE' in col_header:
+        work_state = col_header.index('LCA_CASE_EMPLOYER_STATE')
+    else:
+        print("error message")
 
     top_10_occupations = top10info(status, occupation, row_data)
     top_10_states = top10info(status, work_state, row_data)
 
-    print(top_10_occupations)
-    print(top_10_states)
+    with open('top_10_occupations.txt', 'w') as f:
+        f.write('TOP_OCCUPATIONS;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE')
+        for occupations in top_10_occupations:
+            f.write(','.join(str(v) for v in occupations)+"\n")
+        f.close()
 
+    with open('top_10_states.txt', 'w') as f:
+        f.write('TOP_STATES;NUMBER_CERTIFIED_APPLICATIONS;PERCENTAGE')
+        for states in top_10_states:
+            f.write(','.join(str(v) for v in states)+"\n")
+        f.close()
 
 def top10info(status, group_by_column, row_data):
     """ Returns the top 10 results grouped by :param group_by_column. Sorts alphabetically if there is a tie. """
 
     confirmed_cases = []
     for row in row_data:
-        print(row)
         if row[status] == "CERTIFIED":
             confirmed_cases.append([row[status], row[group_by_column]])
 
@@ -58,8 +77,9 @@ def top10info(status, group_by_column, row_data):
     top10 = percents[:num]
     top10_info = []
     for i in top10:
-        top10_info.append([i[1], i[0], i[2]])
-
+        top10_info.append([i[1].replace('"',''), i[0], str(i[2])+"%"])
+    top10_info.sort(key=lambda row: row[0])
+    top10_info.sort(key=lambda row: row[1], reverse=True)
     return (top10_info)
 
 
